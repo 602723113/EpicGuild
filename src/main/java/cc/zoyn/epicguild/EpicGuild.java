f176f242f08dd686fe83131c2c5b1ca08b01d404
Zoyn <602723113@qq.com>
2017-11-12 20:26:34 +0800

    feat: create project

---
+++ src/main/java/cc/zoyn/epicguild/EpicGuild.java
@@ -0,0 +1,18 @@
+package cc.zoyn.epicguild;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main Class
+ *
+ * @author Zoyn
+ * @since 2017-11-12
+ */
+public class EpicGuild extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
