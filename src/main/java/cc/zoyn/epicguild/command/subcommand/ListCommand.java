b98f4414e0fa4f705960fbecc9b5902dbb37875f
Zoyn <602723113@qq.com>
2017-11-17 00:15:03 +0800

    opt: sync code

--- src/main/java/cc/zoyn/epicguild/command/subcommand/ListCommand.java
+++ src/main/java/cc/zoyn/epicguild/command/subcommand/ListCommand.java
@@ -1,5 +1,6 @@
 package cc.zoyn.epicguild.command.subcommand;
 
+import cc.zoyn.epicguild.manager.GuildManager;
 import cc.zoyn.epicguild.util.SubCommand;
 import org.bukkit.command.CommandSender;
 
@@ -11,7 +12,11 @@ public class ListCommand implements SubCommand {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
+        sender.sendMessage("§6[§eEpicGuild§6]§7----------------------");
+        sender.sendMessage(" ");
+        sender.sendMessage("公会数量: " + GuildManager.getInstance().getGuilds().size());
+        sender.sendMessage("");
+
 
     }
-
 }
--- src/main/java/cc/zoyn/epicguild/command/subcommand/SendCommand.java
+++ src/main/java/cc/zoyn/epicguild/command/subcommand/SendCommand.java
@@ -1,9 +1,7 @@
 package cc.zoyn.epicguild.command.subcommand;
 
-import cc.zoyn.epicguild.api.EpicGuildAPI;
 import cc.zoyn.epicguild.util.SubCommand;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 /**
  * @author Zoyn
@@ -12,9 +10,6 @@ import org.bukkit.entity.Player;
 public class SendCommand implements SubCommand {
     @Override
     public void execute(CommandSender sender, String[] args) {
-        if (sender instanceof Player) {
-            Player player = (Player) sender;
-            EpicGuildAPI.getGuildByPlayer(player);
-        }
+
     }
 }
