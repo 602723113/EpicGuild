f5927bcbbadd94461ada961e12f6fa51bf0bde06
Zoyn <602723113@qq.com>
2017-11-16 23:08:26 +0800

    update: GuildManager

--- src/main/java/cc/zoyn/epicguild/manager/GuildManager.java
+++ src/main/java/cc/zoyn/epicguild/manager/GuildManager.java
@@ -4,6 +4,7 @@ import cc.zoyn.epicguild.dto.Guild;
 import com.google.common.collect.Lists;
 import org.bukkit.entity.Player;
 
+import javax.annotation.Nonnull;
 import java.util.List;
 
 /**
@@ -12,30 +13,40 @@ import java.util.List;
  */
 public class GuildManager implements IGuildManager {
 
-    private static List<Guild> guildList = Lists.newArrayList();
+    private static final List<Guild> guildList = Lists.newArrayList();
 
     @Override
-    public GuildManager addGuild(Guild guild) {
-        return null;
+    public GuildManager addGuild(@Nonnull Guild guild) {
+        guildList.add(guild);
+        return this;
     }
 
     @Override
-    public GuildManager removeGuild(Guild guild) {
-        return null;
+    public GuildManager removeGuild(@Nonnull Guild guild) {
+        if (guildList.contains(guild)) {
+            guildList.remove(guild);
+        }
+        return this;
     }
 
     @Override
     public Guild getGuildByPlayer(Player player) {
-        return null;
+        return guildList.stream()
+                .filter(guild -> guild.isMember(player))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Guild getGuildByName(String guildName) {
-        return null;
+        return guildList.stream()
+                .filter(guild -> guild.getName().equals(guildName))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public List<Guild> getGuilds() {
-        return null;
+        return guildList;
     }
 }
