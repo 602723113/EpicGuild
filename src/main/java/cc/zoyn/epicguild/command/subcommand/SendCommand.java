ddb0d77a14c91622463e7919da0e6c07b132cca1
Zoyn <602723113@qq.com>
2017-11-19 14:18:56 +0800

    update: SendCommand, config.yml

--- src/main/java/cc/zoyn/epicguild/command/subcommand/SendCommand.java
+++ src/main/java/cc/zoyn/epicguild/command/subcommand/SendCommand.java
@@ -1,15 +1,30 @@
 package cc.zoyn.epicguild.command.subcommand;
 
+import cc.zoyn.epicguild.dto.Guild;
+import cc.zoyn.epicguild.manager.ConfigManager;
+import cc.zoyn.epicguild.manager.GuildManager;
 import cc.zoyn.epicguild.util.SubCommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  * @author Zoyn
  * @since 2017-11-13
  */
 public class SendCommand implements SubCommand {
+
     @Override
     public void execute(CommandSender sender, String[] args) {
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
 
+            Guild guild = GuildManager.getInstance().getGuildByPlayer(player);
+            if (guild != null) {
+                guild.sendMessage(ChatColor.translateAlternateColorCodes('&', args[1]));
+            } else {
+                player.sendMessage(ConfigManager.getStringByDefault("CommandMessage.PlayerDontHaveGuild", "&6[&eEpicGuild&6] &c你没有公会!", true));
+            }
+        }
     }
 }
